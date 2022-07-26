d459f0f5a7869a6ba0838fe98345d9823e1ce892
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-26 20:20:07 +0300

    Update PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int counter = 0;
+        if (power >= 0) {
+            while (counter <= power) {
+                System.out.println((int) Math.pow(2, counter));
+                counter++;
+            }
+        } else {
+            System.out.println("too much power");
+        }
     }
 }
