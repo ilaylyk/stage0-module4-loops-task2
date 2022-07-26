028300765d1add1df3fa4c340860dd424621c7c5
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-26 20:19:18 +0300

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+        for (char aChar : chars) {
+            System.out.print(aChar);
+        }
     }
 }
